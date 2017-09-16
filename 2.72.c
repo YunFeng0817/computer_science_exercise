8a7efe6b17cb793a2b0527fb3f69a43021f2bc62
Fitz <f18846188605@gmail.com>
2017-09-17 00:18:47 +0800

    modify the 2.72.c

--- 2.72.c
+++ 2.72.c
@@ -2,13 +2,8 @@
 #include<stdlib.h>
 #include <string.h>
 void copy_int(int val,void *buf,int maxbytes){
-	if(maxbytes-(int)sizeof(val)>=0){
-		printf("%d\n", (int)sizeof(val));
+	if(maxbytes-(int)sizeof(val)>=0){     //此处的坑是：sizeof(val)返回一个size_t类型的数,size_t是无符号类型,maxbytes-sizeof(val)会把maxbytes强转为unsigned类型,相减结果永远大于0，导致if判断永远为真
 		memcpy(buf,(void *)&val,sizeof(val));
-		int *temp;
-		temp = buf;
-		printf("success!\n");
-		printf("%d\n",*temp);
 	}
 }
 
