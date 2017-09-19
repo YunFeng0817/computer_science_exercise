bce811dd5a7367a4accbfdbdf91f0ec1697a8f6b
Fitz <f18846188605@gmail.com>
2017-09-20 00:05:12 +0800

    complete the 2.88.c

--- 2.84.c
+++ 2.84.c
@@ -23,9 +23,7 @@ int flaot_le(float x, float y){
 
     unsigned sx = ux>>31;
     unsigned sy = uy>>31;
-    printf("%0x,%0x\n",sx,sy);
-    printf("%0x,%0x\n",ux&(~INT_MIN),uy&(~INT_MIN));
-    return ((sx|sy==0)&&(ux<=uy)) || ((sx>sy)&&((ux&~INT_MIN)!=0)&&((ux&~INT_MIN)!=0)) || ((ux&~INT_MIN)==0&&(uy&~INT_MIN)==0) || ((sx&sy==1)&&(ux>=uy));
+    return ((sx|sy==0)&&(ux<=uy))||((sx>sy)&&((ux&~INT_MIN)!=0)&&((ux&~INT_MIN)!=0))||((ux&~INT_MIN)==0&&(uy&~INT_MIN)==0)||((sx&sy==1)&&(ux>=uy));
 }
 
 int main(){
---
+++ 2.92.c
@@ -0,0 +1,15 @@
+//
+// Created by Fitz on 2017/9/17.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+float_bits float_negate(float_bits f){
+
+}
+
+int main(){
+    float_negate(3.143);
+    return 0;
+}
